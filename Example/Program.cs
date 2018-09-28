7117f2ae7b2a47bf70da8441553575fd3d982724
id931120repos <id931120@gmail.com>
2018-09-28 18:21:14 +0600

    Function

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Example
 {
@@ -6,7 +7,31 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            //SetCursorPosition(x1, y1);
+            //WriteLine(sym1);
+            Draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            //SetCursorPosition(x2, y2);
+            //WriteLine(sym2);
+            Draw(x2, y2, sym2);
+
+            Pause();
+        }
+
+        static void Pause() => ReadLine();
+
+        static void Draw(int x, int y, char sym)
+        {
+            SetCursorPosition(x, y);
+            WriteLine(sym);
         }
     }
 }
