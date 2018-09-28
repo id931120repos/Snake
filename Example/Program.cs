8dfa999d6e0325d0c939da28acca09243c4f0131
id931120repos <id931120@gmail.com>
2018-09-28 18:45:34 +0600

    Class List

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using static System.Console;
 
 namespace Example
@@ -14,6 +15,35 @@ namespace Example
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+            //List<int> numList = new List<int>
+            //{
+            //    0,
+            //    1
+            //};
+            //numList.Add(2);
+
+            //int x = numList[0];
+            //int y = numList[1];
+            //int z = numList[2];
+
+            //foreach (int i in numList)
+            //{
+            //    WriteLine(i);
+            //}
+
+            //numList.RemoveAt(0);
+
+            //foreach (int i in numList)
+            //{
+            //    WriteLine(i);
+            //}
+
+            List<Point> pList = new List<Point>
+            {
+                p1
+            };
+            pList.Add(p2);
 
             Pause();
         }
