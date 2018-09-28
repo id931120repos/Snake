bb563a9c5c6fc1e855be7104e2cbfa951e7f7386
id931120repos <id931120@gmail.com>
2018-09-28 18:39:01 +0600

    Constructor

--- Example/Point.cs
+++ Example/Point.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using static System.Console;
+
 namespace Example
 {
     public class Point
     {
-        public Point()
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point(int x, int y, char sym)
         {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+
+        public void Draw()
+        {
+            SetCursorPosition(x, y);
+            WriteLine(sym);
         }
     }
 }
--- Example/Program.cs
+++ Example/Program.cs
@@ -5,33 +5,17 @@ namespace Example
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-
-            //SetCursorPosition(x1, y1);
-            //WriteLine(sym1);
-            Draw(x1, y1, sym1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            //SetCursorPosition(x2, y2);
-            //WriteLine(sym2);
-            Draw(x2, y2, sym2);
-
-            Pause();
-        }
-
         static void Pause() => ReadLine();
 
-        static void Draw(int x, int y, char sym)
+        static void Main(string[] args)
         {
-            SetCursorPosition(x, y);
-            WriteLine(sym);
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            Pause();
         }
     }
 }
